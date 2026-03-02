0f1e1540b8139a2b2569733d34d9cf2bf0d65fb0
Adam Horscraft <adam.horscraft@gmail.com>
2026-03-02 14:23:04 +0000

    fix(app): added d.ts for processenv types

---
+++ app/env.d.ts
@@ -0,0 +1,12 @@
+declare namespace NodeJS {
+  interface ProcessEnv {
+    EXPO_PUBLIC_API_URL?: string;
+    EXPO_PUBLIC_STREAMING_URL?: string;
+    EXPO_PUBLIC_COGNITO_USER_POOL_ID?: string;
+    EXPO_PUBLIC_COGNITO_CLIENT_ID?: string;
+    EXPO_PUBLIC_COGNITO_REGION?: string;
+    EXPO_PUBLIC_ANTHROPIC_API_KEY?: string;
+    EXPO_PUBLIC_POSTHOG_API_KEY?: string;
+    EXPO_PUBLIC_POSTHOG_HOST?: string;
+  }
+}
